3795dc92adc38c021fb5eb1d4b1c0384079ee380
Jack <jack@spur.us>
2024-08-07 20:08:15 +0000

    Removed test script

--- scripts/testdeploy.sh
+++
@@ -1,2 +0,0 @@
-gcloud builds submit --tag gcr.io/jack-testing-5150/spur-enrichment-for-maltego
-gcloud run deploy staging-spur-enrichment-for-maltego --image gcr.io/jack-testing-5150/spur-enrichment-for-maltego --platform managed --region=us-east1
